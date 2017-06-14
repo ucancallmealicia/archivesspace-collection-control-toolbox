7d2619a526e7ab35edc565385034bc4c30570156
ucancallmealicia <adetelich@gmail.com>
2017-06-14 15:52:44 -0400

    Create create_container_profiles.py

--- container_profiles/create_container_profiles.py
+++ container_profiles/create_container_profiles.py
@@ -5,6 +5,7 @@ import getpass
 import pprint
 import csv
 
+#login procedure - enter ArchivesSpace API url, credentials
 def login (api_url, username, password):
     '''This function logs into the ArchivesSpace REST API returning an acccess token'''
     auth = requests.post(api_url+'/users/'+username+'/login?password='+password).json()
@@ -26,11 +27,16 @@ if __name__ == '__main__':
     else:
         print('Ooops! something went wrong')
 
+#The magic happens here...
+#Enter full path to spreadsheet where container profile data is stored
 input_csv = input("Please enter path to input CSV: ")
 
+#Opens spreadsheet, reads data into memory
 with open(input_csv, 'r', encoding='utf-8') as csvfile:
     csvin = csv.reader(csvfile)
+    #skip header row
     next(csvin, None)
+    #loops through spreadsheet one row at a time, adding container profile
     for row in csvin:
         name = row[0]
         extent_dimension = row[1]
@@ -38,11 +44,14 @@ with open(input_csv, 'r', encoding='utf-8') as csvfile:
         width = row[3]
         depth = row[4]
         dimension_units = row[5]
+        #takes data from spreadsheet and builds JSON
         new_container_profile = {'jsonmodel_type': 'container_profile', 'name': name,
                                  'extent_dimension': extent_dimension, 'height': height,
                                  'width': width, 'depth': depth, 'dimension_units': dimension_units}
         container_profile_data = json.dumps(new_container_profile)
+        #Posts JSON to ArchivesSpace
         create_profile = requests.post(api_url + '/container_profiles', headers=headers, data=container_profile_data).json()
+        #Prints what is happening to IDLE window - will add an error log as well
         print(create_profile)
-#add outfile for error logging
+        
 print('All Done!')
