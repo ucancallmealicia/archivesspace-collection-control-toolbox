4796f147671fb24266dd33b2408859ecbafb732d
ucancallmealicia <adetelich@gmail.com>
2017-07-11 12:24:10 -0400

    added URI

--- location_profiles/get_location_profiles.sql
+++ location_profiles/get_location_profiles.sql
@@ -3,5 +3,6 @@ SELECT lp.name
     , lp.width
     , lp.height
 	, ev.value AS dimension_units
+    , CONCAT('/location_profiles/', lp.id) AS location_profile_URI
 FROM location_profile lp
 LEFT JOIN enumeration_value ev on ev.id = lp.dimension_units_id
