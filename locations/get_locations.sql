a5506a964c7bf8da22d46aaa081fe772696c6ab7
ucancallmealicia <adetelich@gmail.com>
2017-07-11 12:27:49 -0400

    added location profile business

--- locations/get_locations.sql
+++ locations/get_locations.sql
@@ -1,13 +1,16 @@
-SELECT title
-	, building
-    , floor
-    , room
-    , area
-	, coordinate_1_label
-    , coordinate_1_indicator
-	, coordinate_2_label
-    , coordinate_2_indicator
-    , coordinate_3_label
-    , coordinate_3_indicator
-    , CONCAT('/locations/', id) as location_URI
-FROM location
+SELECT l.title
+	, l.building
+    , l.floor
+    , l.room
+    , l.area
+	, l.coordinate_1_label
+    , l.coordinate_1_indicator
+	, l.coordinate_2_label
+    , l.coordinate_2_indicator
+    , l.coordinate_3_label
+    , l.coordinate_3_indicator
+    , CONCAT('/locations/', l.id) as location_URI
+    , CONCAT('/location_profiles', lp.id) as location_profile_URI
+FROM location l
+LEFT JOIN location_profile_rlshp lpr on lpr.location_id = l.id
+LEFT JOIN location_profile lp on lp.id = lpr.location_profile_id
