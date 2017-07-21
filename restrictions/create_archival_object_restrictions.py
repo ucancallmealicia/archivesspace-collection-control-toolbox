969d37d94adc677914b87f9842dbce06ca2bf61f
ucancallmealicia <adetelich@gmail.com>
2017-07-21 12:44:22 -0400

    Update create_archival_object_restrictions.py

--- restrictions/create_archival_object_restrictions.py
+++ restrictions/create_archival_object_restrictions.py
@@ -66,7 +66,6 @@ with open(input_csv, 'r', encoding='utf-8') as csvfile, open(outfile, 'a') as tx
         for key, value in ao_update.items():
             if key == 'status':
                 txtout.write('%s:%s\n' % (key, value))
-                x = x +1
             if key == 'uri':
                 txtout.write('%s:%s\n' % (key, value) + '\n')
             if key == 'error':
